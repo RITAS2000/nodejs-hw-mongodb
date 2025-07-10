86a04de3d8848793a777fe6a55b9ebe469a725a6
Yuliya Lev <yuliya.lev.2000@gmail.com>
2025-07-10 19:50:42 +0300

    hw3

---
+++ src/controllers/contacts.js
@@ -0,0 +1,112 @@
+import createHttpError from 'http-errors';
+
+import {
+  createContact,
+  getAllContacts,
+  getContactById,
+  updateContact,
+  deleteContact,
+} from '../services/contacts.js';
+
+export async function showContactsController(req, res) {
+  const contacts = await getAllContacts().catch((error) => {
+    return res.status(500).json({
+      status: 500,
+      message: 'Error retrieving contacts',
+      error: error.message,
+    });
+  });
+
+  if (!contacts) return;
+
+  res
+    .status(200)
+    .set('Content-Type', 'application/json')
+    .send(
+      JSON.stringify(
+        {
+          status: 200,
+          message: 'Successfully found contacts!',
+          data: contacts,
+        },
+        null,
+        2,
+      ),
+    );
+}
+
+export async function showContactByIdController(req, res) {
+  const { contactId } = req.params;
+
+  const contact = await getContactById(contactId);
+
+  if (contact === null) {
+    throw new createHttpError.NotFound('Contact not found');
+  }
+
+  res
+    .status(200)
+    .set('Content-Type', 'application/json')
+    .send(
+      JSON.stringify(
+        {
+          status: 200,
+          message: `Successfully found contact with id ${contactId}!`,
+          data: contact,
+        },
+        null,
+        2,
+      ),
+    );
+}
+
+export async function createNewContactController(req, res) {
+  const contact = await createContact(req.body);
+
+  res
+    .status(201)
+    .set('Content-Type', 'application/json')
+    .send(
+      JSON.stringify(
+        {
+          status: 201,
+          message: 'Successfully created a contact!',
+          data: contact,
+        },
+        null,
+        2,
+      ),
+    );
+}
+
+export async function updateContactController(req, res) {
+  const result = await updateContact(req.params.contactId, req.body);
+
+  if (result === null) {
+    throw new createHttpError.NotFound('Contact not found');
+  }
+
+  res
+    .status(200)
+    .set('Content-Type', 'application/json')
+    .send(
+      JSON.stringify(
+        {
+          status: 200,
+          message: 'Successfully patched a contact!',
+          data: result,
+        },
+        null,
+        2,
+      ),
+    );
+}
+
+export async function deleteContactController(req, res) {
+  const result = await deleteContact(req.params.contactId);
+
+  if (result === null) {
+    throw new createHttpError.NotFound('Contact not found');
+  }
+  res.status(204).end();
+}
--- src/server.js
+++ src/server.js
@@ -2,8 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
 import { getEnvVar } from './utils/getEnvVar.js';
-import { showContacts } from './controllers/contactsController.js';
-import { showContactById } from './controllers/showContactById.js';
+import contactsRouters from './routers/contacts.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
 const PORT = Number(getEnvVar('PORT', '3000')) || 3000;
 
@@ -19,12 +20,9 @@ export function setupServer() {
       },
     }),
   );
-  app.get('/contacts', showContacts);
-  app.get('/contacts/:contactId', showContactById);
-
-  app.use((req, res) => {
-    res.status(404).json({ message: 'Not found' });
-  });
+  app.use('/contacts', contactsRouters);
+  app.use(notFoundHandler);
+  app.use(errorHandler);
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
